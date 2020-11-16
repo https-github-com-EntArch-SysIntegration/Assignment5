e644b980359836d1d6f13544d1229195f223fd11
kbalusupati <kbalusupati@unomaha.edu>
2020-11-16 13:01:23 -0600

    smtp change

--- rentalAppPrj/settings.py
+++ rentalAppPrj/settings.py
@@ -149,7 +149,7 @@ django_heroku.settings(locals())
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'krish.veni2591@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('Sunsilk@123')
+EMAIL_HOST_PASSWORD = 'Sunsilk@123'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'krish.veni2591@gmail.com'
